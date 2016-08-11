577f1e0868254c7fb8d9abd14ab2808513d33381
Takehito Amanuma <amanuma.takehito@tis.co.jp>
2016-08-12 08:53:55 +0900

    add test for initial state. still error.

--- components.spec.js
+++ components.spec.js
@@ -4,6 +4,7 @@ import { shallow, mount } from 'enzyme';
 import { BeerListContainer, BeerList, InputArea } from './components'
 
 describe('BeerListContainer', () => {
+    
     it('should render InputArea and BeerList', () => {
         const wrapper = shallow(<BeerListContainer/>);
         expect(wrapper.containsAllMatchingElements([
@@ -11,4 +12,10 @@ describe('BeerListContainer', () => {
             <BeerList/>
         ])).to.equal(true);
     });
+    
+    it('should start with an empty list', () => {
+        const wrapper = shallow(<BeerListContainer/>);
+        expect(wrapper.state('beer')).to.equal([]);
+    });
+    
 });
